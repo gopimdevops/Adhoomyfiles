acded06c798ef46d7f600bac602df77d00a7f79a
gopimdevops <55191704+gopimdevops@users.noreply.github.com>
2019-09-16 21:29:43 +0530

    Getting Cred

--- vmcreate.ps1
+++ vmcreate.ps1
@@ -1,3 +1,3 @@
-$Credential = Aathvik@2017
+$Credential = ConvertTo-SecureString Aathvik@2017 -AsPlainText -Force
 Connect-AzAccount -Credential $Credential
 New-AzResourceGroupDeployment -Name AdhooVMdeployment –ResourceGroupName adhooRG001 –TemplateUri https://github.com/gopimdevops/Adhoomyfiles/blob/dev/templates.json  –TemplateParameterUri https://github.com/gopimdevops/Adhoomyfiles/blob/dev/adhooparam.json 
